9d2e64b4eb56874aa37b7495df9d8591f9a7d4dc
Augusto Goncalves <augusto.goncalves@autodesk.com>
2018-03-02 11:20:05 -0800

    socket

--- start.js
+++ start.js
@@ -9,4 +9,19 @@ var server = app.listen(app.get('port'), function () {
 
   console.log('Starting at ' + (new Date()).toString());
   console.log('Server listening on port ' + server.address().port);
+
+  var io = require('socket.io').listen(server);
+  io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+      console.log('user disconnected');
+    });
+    socket.on('join', function (room) {
+      console.log('join: ' + room);
+      socket.join(room.viewableId);
+    });
+    socket.on('statechanged', function (data) {
+      socket.to(data.viewableId).emit('newstate', data.state);
+    });
+  });
 });
